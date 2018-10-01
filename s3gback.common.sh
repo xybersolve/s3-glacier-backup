f7a7bb218f995791dfc51fb0fdf4ec8c917f1618
xybersolve <1531070+xybersolve@users.noreply.github.com>
2018-09-30 18:59:22 -0700

    add sns notifications & common file

---
+++ s3gback.common.sh
@@ -0,0 +1,7 @@
+declare -r REGION='us-west-2'
+declare -r ACCOUNT_ID='734741078887'
+declare -r DEFAULT_MESSAGE='Back up performed'
+declare ADMIN_EMAIL='xybersolve@gmail.com'
+declare TOPIC_NAME='S3-Glacier-Backup'
+declare TOPIC_SUBJECT='S3-Glacier-Backup'
+declare SUBSCRIPTION_ID='7b399f3f-8ac4-4fb7-82a3-4447274632e4'
